d633a21c48b2f0895cb65dbd4156cb5755e074a7
Saul Hernandez <104327731+saulhdez28@users.noreply.github.com>
2022-04-24 16:58:08 -0600

    Pureba
    first commit

---
+++ ProjectoFinalClienteServidor/src/projectofinalclienteservidor/ProjectoFinalClienteServidor.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectofinalclienteservidor;
+
+/**
+ *
+ * @author Saul Hernandez
+ */
+public class ProjectoFinalClienteServidor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Hola esto es una prueba");
+        System.out.println("sadfasf");
+        // TODO code application logic here
+    }
+    
+}
